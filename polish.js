5254bf464fcbac81780955615633e35760c7b299
Zoli Kahan <zolikahan@gmail.com>
2013-05-10 21:16:22 -0500

    polish.js

---
+++ polish.js
@@ -0,0 +1,346 @@
+/* Polish.js v0.1.0 | Zolmeister | MIT License*/
+(function () {
+    var global = this
+
+    global.strings = {
+        letters: 'abcdefghijklmnopqrstuvwxyz',
+        letters_all: 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+        digits: '0123456789'
+    }
+
+    global.abs = Math.abs.bind(Math)
+
+    var mathMin = Math.min.bind(Math)
+    Object.defineProperty(Math, 'min', {
+        get: function () {
+            return function () {
+                //do minimum of arrays
+                if (arguments[0] instanceof Array) {
+                    //minumun of 1 array
+                    if (arguments.length == 1) {
+                        return mathMin.apply(Math, arguments[0])
+                    } else {
+                        //comparison of multiple arrays
+                        var min = arguments[0]
+                        for (var i = 1, l = arguments.length; i < l; i++) {
+                            if (arguments[i] < min) min = arguments[i]
+                        }
+                        return min
+                    }
+                }
+                //normal
+                else {
+                    return mathMin.apply(Math, arguments)
+                }
+            }.bind(Math);
+        }
+    })
+
+    var mathMax = Math.max.bind(Math)
+    Object.defineProperty(Math, 'max', {
+        get: function () {
+            return function () {
+                //do minimum of arrays
+                if (arguments[0] instanceof Array) {
+                    //minumun of 1 array
+                    if (arguments.length <= 1) {
+                        return mathMax.apply(Math, arguments[0])
+                    } else {
+                        //comparison of multiple arrays
+                        var max = arguments[0]
+                        for (var i = 1, l = arguments.length; i < l; i++) {
+                            if (arguments[i] > max) max = arguments[i]
+                        }
+                        return max
+                    }
+                }
+                //normal
+                else {
+                    return mathMax.apply(Math, arguments)
+                }
+            }.bind(Math);
+        }
+    })
+
+    //return random int between min and max inclusive
+    global.Math.randInt = function randInt(min, max) {
+        if (!max) {
+            max = min
+            min = 0
+        }
+        return min + Math.floor(Math.random() * (max - min + 1))
+    }.bind(Math)
+
+    var factorialMemoize = []
+    global.Math.factorial = function factorial(n) {
+        if (n == 0 || n == 1) return 1;
+        if (factorialMemoize[n] > 0) return factorialMemoize[n];
+        else return factorialMemoize[n] = factorial(n - 1) * n;
+    }
+
+    //http://www.javascripter.net/faq/numberisprime.htm
+    global.Math.isPrime = function (n) {
+        if (isNaN(n) || !isFinite(n) || n % 1 || n < 2) return false;
+        if (n == this.leastFactor(n)) return true;
+        return false;
+    }.bind(Math)
+
+    //http://www.javascripter.net/faq/numberisprime.htm
+    global.Math.leastFactor = function (n) {
+        if (isNaN(n) || !isFinite(n)) return NaN;
+        if (n == 0) return 0;
+        if (n % 1 || n * n < 2) return 1;
+        if (n % 2 == 0) return 2;
+        if (n % 3 == 0) return 3;
+        if (n % 5 == 0) return 5;
+        var m = Math.sqrt(n);
+        for (var i = 7; i <= m; i += 30) {
+            if (n % i == 0) return i;
+            if (n % (i + 4) == 0) return i + 4;
+            if (n % (i + 6) == 0) return i + 6;
+            if (n % (i + 10) == 0) return i + 10;
+            if (n % (i + 12) == 0) return i + 12;
+            if (n % (i + 16) == 0) return i + 16;
+            if (n % (i + 22) == 0) return i + 22;
+            if (n % (i + 24) == 0) return i + 24;
+        }
+        return n;
+    }.bind(Math)
+
+    global.itertools = global.itertools || {}
+
+    global.itertools.combinations = function (list, len, replacement) {
+        var result = [];
+        var fn = function (prefix, remaining) {
+            if (len && prefix.length >= len) return
+            for (var i = 0, l = remaining.length; i < l; i++) {
+                if (!len || prefix.length + 1 === len) result.push(prefix.concat(remaining[i]));
+                fn(prefix.concat(remaining[i]), replacement ? remaining : remaining.slice(i + 1));
+            }
+        }
+
+        fn([], list);
+        return result;
+    }.bind(global.itertools)
+
+    global.itertools.combinations_with_replacement = function (list, len) {
+        return this.combinations(list, len, true)
+    }.bind(global.itertools)
+
+    //http://stackoverflow.com/questions/15724986/get-all-permutations-of-an-array-of-length-l-using-iteration
+    global.itertools.permutations = function (list) {
+        var perms = [],
+            used = []
+        var fn = function permute(input) {
+            var ch;
+            for (var i = 0, l = input.length; i < l; i++) {
+                ch = input.splice(i, 1)[0];
+                used.push(ch);
+                if (input.length === 0) {
+                    perms.push(used.slice());
+                }
+                permute(input);
+                input.splice(i, 0, ch);
+                used.pop();
+            }
+            return perms
+        }
+        return fn(list)
+    }
+
+    //http://underscorejs.org/docs/underscore.html
+    global.range = function (start, stop, step) {
+        if (arguments.length <= 1) {
+            stop = start || 0
+            start = 0
+        }
+        step = arguments[2] || 1
+
+        var len = Math.max(Math.ceil((stop - start) / step), 0)
+        var idx = 0
+        var range = new Array(len)
+
+        while (idx < len) {
+            range[idx++] = start
+            start += step
+        }
+
+        return range
+    }
+
+    global.clone = function (obj) {
+        return JSON.parse(JSON.stringify(obj));
+    }
+
+    //http://stackoverflow.com/questions/4856717/javascript-equivalent-of-pythons-zip-function
+    global.zip = function zip() {
+        //if 2d list passed in, bubble it down
+        if (arguments.length === 1 && arguments[0] instanceof Array && arguments[0][0] instanceof Array) {
+            return zip.apply(this, arguments[0])
+        }
+
+        var args = [].slice.call(arguments);
+        var shortest = args.length == 0 ? [] : args.reduce(function (a, b) {
+            return a.length < b.length ? a : b
+        });
+
+        return shortest.map(function (_, i) {
+            return args.map(function (array) {
+                return array[i]
+            })
+        });
+    }
+
+    global.sum = function sum() {
+        if (arguments.length === 1 && arguments[0] instanceof Array) {
+            return sum.apply(this, arguments[0])
+        }
+        var cnt = 0
+        for (var i = 0, l = arguments.length; i < l; i++) {
+            cnt += arguments[i]
+        }
+        return cnt
+    }
+
+    Object.defineProperty(Array.prototype, 'choice', {
+        get: function () {
+            return function () {
+                return this[global.Math.randInt(this.length - 1)]
+            }
+        }
+    })
+
+    Object.defineProperty(String.prototype, 'choice', {
+        get: function () {
+            return function () {
+                return this[global.Math.randInt(this.length - 1)]
+            }
+        }
+    })
+
+    function shuffle() {
+        var i = this.length,
+            j, tempi, tempj;
+        if (i === 0) return this;
+        while (--i) {
+            j = global.Math.randInt(i)
+            tempi = this[i];
+            tempj = this[j];
+            this[i] = tempj;
+            this[j] = tempi;
+        }
+        return this
+    }
+
+    //http://stackoverflow.com/questions/2450954/how-to-randomize-a-javascript-array
+    Object.defineProperty(Array.prototype, 'shuffle', {
+        get: function () {
+            return shuffle
+        }
+    })
+
+    Object.defineProperty(String.prototype, 'shuffle', {
+        get: function () {
+            return function () {
+                return shuffle.apply(this.split('')).join('')
+            }
+        }
+    })
+
+    var oldPop = Array.prototype.pop;
+    Object.defineProperty(Array.prototype, 'pop', {
+        get: function () {
+            return function (index) {
+                if (index && this[index]) {
+                    return this.splice(index, 1)[0]
+                }
+                return oldPop.call(this)
+            }
+        }
+    })
+
+    Object.defineProperty(Array.prototype, 'remove', {
+        get: function () {
+            return function (elem) {
+                this.pop(this.indexOf(elem))
+                return this
+            }
+        }
+    })
+
+    Object.defineProperty(Array.prototype, 'insert', {
+        get: function () {
+            return function (index, elem) {
+                this.splice(index, 0, elem)
+                return this
+            }
+        }
+    })
+
+    Object.defineProperty(Array.prototype, '-1', {
+        get: function () {
+            return this[this.length - 1]
+        }
+    })
+
+    Object.defineProperty(String.prototype, '-1', {
+        get: function () {
+            return this[this.length - 1]
+        }
+    })
+
+    Object.defineProperty(String.prototype, 'reverse', {
+        get: function () {
+            return function () {
+                return this.split('').reverse().join('')
+            }
+        }
+    })
+
+    function select(sel) {
+        //single element selection
+        if (typeof sel === 'number' || sel.indexOf(':') === -1) {
+            var index = (this.length + parseInt(sel,10)) % this.length
+            return this[index]
+        }
+        sel = sel.split(':').map(function (e) {
+            return parseInt(e,10)
+        })
+        var start = parseInt(sel[0],10) || 0
+        var end = parseInt(sel[1],10) || this.length
+        var step = parseInt(sel[2],10) || 1
+        if (step < 0) {
+            if (start < end) {
+                start *= -1
+                start -= 1
+                end *= -1
+                end -= 1
+            }
+        }
+
+        if (end < 0 && start >= 0) {
+            end = (this.length + end) % this.length
+        }
+
+        var ret = []
+        var indicies = global.range(start, end, step)
+        for (var i = 0, l = indicies.length; i < l; i++) {
+            var index = indicies[i]
+            ret.push(select.bind(this)(index))
+        }
+        if (this instanceof Array) return ret
+        return ret.join('')
+    }
+
+    Object.defineProperty(Array.prototype, 'z', {
+        get: function () {
+            return select
+        }
+    })
+
+    Object.defineProperty(String.prototype, 'z', {
+        get: function () {
+            return select
+        }
+    })
+})();
---
+++ polish.spec.js
@@ -0,0 +1,160 @@
+require('./polish')
+
+describe("Zappy", function () {
+    it("has strings", function () {
+        expect(typeof strings).toBe('object')
+    })
+    it("scopes up abs", function () {
+        expect(typeof abs).toBe('function')
+    })
+    it("adds range", function () {
+        expect(range(1, 20, 2) instanceof Array).toBe(true)
+    })
+    it("clones", function () {
+        var obj = {
+            'a': {
+                'c': 1
+            }
+        }
+        expect(clone(obj) === obj).toBe(false)
+        expect(clone(obj).toString() === obj.toString()).toBe(true)
+    })
+    it("zips", function () {
+        //TODO: actually test zipping
+        var arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+        expect(zip(zip(arr)).toString() === arr.toString()).toBe(true)
+    })
+    it("sums", function () {
+        expect(sum([1, 2, 3])).toBe(6)
+        expect(sum(1, 2, 3)).toBe(6)
+    })
+    describe("Arrays", function(){
+        it("pops indexes", function(){
+            var list = [1,2,3,4,5]
+            expect(list.pop()).toBe(5)
+            expect(list.pop(1)).toBe(2)
+        })
+        it("removes elements", function(){
+            var list = [1,2,3,4,5]
+            expect(list.remove(2).toString()).toBe([1,3,4,5].toString())
+        })
+        it("inserts elements", function(){
+            var list = [1,2,3,4,5]
+            expect(list.insert(2,5).toString()).toBe([1,2,5,3,4,5].toString())
+        })
+            
+    })
+    describe("Maths", function () {
+        it("returns mins of multiple numbers", function () {
+            expect(Math.min(1, 2)).toBe(1)
+        })
+        it("returns mins from a single array", function () {
+            expect(Math.min([1, 2])).toBe(1)
+        })
+        it("returns mins of multiple arrays", function () {
+            expect(Math.min([1, 2], [2, 3]) instanceof Array).toBe(true)
+        })
+        it("returns max of multiple numbers", function () {
+            expect(Math.max(1, 2)).toBe(2)
+        })
+        it("returns max from a single array", function () {
+            expect(Math.max([1, 2])).toBe(2)
+        })
+        it("returns max of multiple arrays", function () {
+            expect(Math.max([1, 2], [2, 3]) instanceof Array).toBe(true)
+        })
+        it("factorials", function () {
+            expect(Math.factorial(10)).toBe(3628800)
+        })
+        it("primes", function () {
+            expect(Math.isPrime(23)).toBe(true)
+            expect(Math.isPrime(24)).toBe(false)
+        })
+    })
+    describe("Randoms", function () {
+        //TODO: actually test these
+        it("gets random int", function () {
+            expect(Math.randInt(100)).toBeLessThan(101)
+        })
+        it("gets random choice from list", function () {
+            expect([1, 2, 3].choice()).toBeGreaterThan(0)
+        })
+        it("gets random choice from string", function () {
+            expect('12345'.choice()).toBeGreaterThan(0)
+        })
+        it("shuffles lists in place", function () {
+            var unshuffled = range(1000)
+            expect(unshuffled.toString()).not.toEqual(unshuffled.shuffle().toString())
+        })
+        it("get shuffled strings", function () {
+            var a = range(1000).join('')
+            expect(a.toString()).not.toEqual(a.shuffle().toString())
+        })
+    })
+    describe("itertools", function () {
+        it("combos", function () {
+            expect(itertools.combinations([1, 2, 3, 4, 5], 3).length).toBe(10)
+        })
+        it("combos with replacement", function () {
+            expect(itertools.combinations_with_replacement([1, 2, 3, 4, 5], 3).length).toBe(125)
+        })
+        it("permutes", function () {
+            expect(itertools.permutations([1, 2, 3, 4]).length).toBe(24)
+        })
+    })
+    describe("reverse functions", function(){
+        it("reverses strings", function(){
+            var str = "abcdef"
+            expect(str.reverse()).toBe("fedcba")
+        })
+    })
+    describe("python selectors", function(){
+        //TODO: finish adding list tests
+        var list = [1,2,3,4,5]
+        var str = 'abcdef'
+        it("selectcs -1", function(){
+            expect(list[-1]).toBe(list.z(-1))
+            expect(str[-1]).toBe('f')
+        })
+        it("selects positive", function(){
+            expect(list.z('1')).toBe(2)
+            expect(str.z('1')).toBe('b')
+        })
+        it("selects negative", function(){
+            expect(list.z('-1')).toBe(5)
+            expect(str.z('-1')).toBe('f')
+        })
+        it("selects 0", function(){
+            expect(list.z('0')).toBe(1)
+            expect(str.z('0')).toBe('a')
+        })
+        it("selects positive range without end", function(){
+            expect(list.z('1:').toString()).toBe(list.slice(1).toString())
+            expect(str.z('1:')).toBe(str.substring(1))
+        })
+        it("selects positive range without beginning", function(){
+            expect(list.z(':2').toString()).toBe(list.slice(0,2).toString())
+            expect(str.z(':2')).toBe(str.substring(0,2))
+        })
+        it("selects positive range", function(){
+            expect(list.z('1:3').toString()).toBe(list.slice(1,3).toString())
+            expect(str.z('1:3')).toBe(str.substring(1,3))
+        })
+        it("selects negative range", function(){
+            expect(str.z('-3:-1')).toBe('de')
+        })
+        it("selects mixed range", function(){
+            expect(str.z('2:-1')).toBe('cde')
+        })
+        it("reverses", function(){
+            expect(str.z('::-1')).toBe('fedcba')
+        })
+        it("selects positive range and reverses", function(){
+            expect(str.z('3:1:-1')).toBe('dc')
+        })
+    })
+})
+    
+    
+    
+    
